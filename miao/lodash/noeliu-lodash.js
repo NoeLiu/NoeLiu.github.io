de5f6d74e2576dbbe8a18cd67ef6f426e093de0a
NoeLiu <345475319@qq.com>
2018-07-18 20:32:57 +0800

    Update noeliu-lodash.js

--- miao/lodash/noeliu-lodash.js
+++ miao/lodash/noeliu-lodash.js
@@ -10,7 +10,7 @@ var noeliu = {
     compact: function(array){
         var result = []
         for(var i = 0;i < array.length;i++){
-            if(array[i] !==false && array[i] !==null && array[i] !==0 && array[i] !=='' &&array[i] !==undefined&&array[i] !==NaN){
+            if(array[i] !==false && array[i] !==null && array[i] !==0 && array[i] !=='' &&array[i] !==undefined&&array[i] === array[i]){
                 result.push(array[i])
             }
         }
