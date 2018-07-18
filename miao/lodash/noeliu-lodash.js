69072f90f57c6dfd34e73f28391534bd526977cf
NoeLiu <345475319@qq.com>
2018-07-18 20:23:01 +0800

    Update noeliu-lodash.js

--- miao/lodash/noeliu-lodash.js
+++ miao/lodash/noeliu-lodash.js
@@ -1,7 +1,20 @@
 
 var noeliu = {
-    chunk: function(array, size = 1) {
-
+    chunk: function(array, size) {
+        var result = []
+        for(var i = 0;i < array.length;i+=size){
+            result.push(array.slice(i,i+size))        
+        }
+        return result
+    },
+    compact: function(array){
+        var result = []
+        for(var i = 0;i < array.length;i++){
+            if(array[i] !==false && array[i] !==null && array[i] !==0 && array[i] !=='' &&array[i] !==undefined&&array[i] !==NaN){
+                result.push(array[i])
+            }
+        }
+        return result
     },
     property: function(propName){
         return function(obj){
@@ -9,4 +22,3 @@ var noeliu = {
         }
     }
 }
-
