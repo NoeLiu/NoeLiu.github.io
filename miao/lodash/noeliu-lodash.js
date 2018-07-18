1670b2dd9f71e5604b0c29659d82e24f4b8d5a7b
NoeLiu <345475319@qq.com>
2018-07-18 19:49:19 +0800

    Add files via upload

--- miao/lodash/noeliu-lodash.js
+++ miao/lodash/noeliu-lodash.js
@@ -2,6 +2,11 @@
 var noeliu = {
     chunk: function(array, size = 1) {
 
+    },
+    property: function(propName){
+        return function(obj){
+            return obj[propName]
+        }
     }
 }
 
